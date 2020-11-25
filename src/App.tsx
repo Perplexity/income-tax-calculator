d075e1874a3ed0aa825fdcb64aff7ea82d274978
Perplexity <ryan4965@hotmail.co.uk>
2020-11-25 19:31:36 +0000

    Code comments + Logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Calculator from './features/calculator/Calculator';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Calculator/>
+        <img src={logo} className="igloo-logo" />
+        <Calculator />
       </header>
     </div>
   );
--- src/features/calculator/calculatorSlice.ts
+++ src/features/calculator/calculatorSlice.ts
@@ -2,16 +2,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TaxBand, taxBands } from '../../types/TaxBand';
 
+//The interface for our state object.
 interface CalculatorState {
     income: number
-    taxBand: TaxBand | null | undefined
+    taxBand: TaxBand | null | undefined //Value can also be null or undefined, as default value is null, and our 'getTaxBand' function can return 'undefined'
 }
-
+//Our initial state object
 const initialState: CalculatorState = {
     income: 0,
     taxBand: null
 };
-
+//Our 'slice' is what provides our reducers and sets the initial state.
 export const calculatorSlice = createSlice({
     name: 'calculator',
     initialState,
@@ -21,12 +22,12 @@ export const calculatorSlice = createSlice({
         },
         getTaxBand: state => {
             const { income } = state;
-            const taxBand = taxBands.find(band => income >= band.minThreshold && income <= band.maxThreshold);
+            const taxBand = taxBands.find(band => income >= band.minThreshold && income <= band.maxThreshold); //Find a tax band by checking if our income fits between the min and max threshold.
             state.taxBand = taxBand;
         }
     },
 });
 
-export const { setIncome, getTaxBand } = calculatorSlice.actions;
+export const { setIncome, getTaxBand } = calculatorSlice.actions; //Export actions to be used for dispatching.
 
-export default calculatorSlice.reducer;
+export default calculatorSlice.reducer; //Export the reducer to be used in our store.
